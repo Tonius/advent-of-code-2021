dd4b1fc56c4d747059c67f1722903e1875359984
Ton Broekhuizen <tonbro96@gmail.com>
2021-12-02 21:43:59 +0100

    Nicer

--- 02.py
+++ 02.py
@@ -1,18 +1,21 @@
+commands: list[tuple[str, int]] = []
 with open("02-input.txt") as input_file:
-    commands = [line.removesuffix("\n") for line in input_file]
+    for line in input_file:
+        command, value = line.split()
+        commands.append((command, int(value)))
 
 print("\npart 1")
 
 horizontal_pos: int = 0
 depth: int = 0
 
-for command in commands:
-    if command.startswith("forward"):
-        horizontal_pos += int(command.removeprefix("forward "))
-    elif command.startswith("down"):
-        depth += int(command.removeprefix("down "))
-    elif command.startswith("up"):
-        depth -= int(command.removeprefix("up "))
+for command, value in commands:
+    if command == "forward":
+        horizontal_pos += value
+    elif command == "down":
+        depth += value
+    elif command == "up":
+        depth -= value
 
 print(f"horizontal position: {horizontal_pos}")
 print(f"depth: {depth}")
@@ -24,13 +27,12 @@ aim: int = 0
 horizontal_pos: int = 0
 depth: int = 0
 
-for command in commands:
-    if command.startswith("down"):
-        aim += int(command.removeprefix("down "))
-    elif command.startswith("up"):
-        aim -= int(command.removeprefix("up "))
-    elif command.startswith("forward"):
-        value = int(command.removeprefix("forward "))
+for command, value in commands:
+    if command == "down":
+        aim += value
+    elif command == "up":
+        aim -= value
+    elif command == "forward":
         horizontal_pos += value
         depth += aim * value
 
