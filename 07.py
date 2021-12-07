861880d346451e43adfb500b60095b032350bf2c
Ton Broekhuizen <tonbro96@gmail.com>
2021-12-07 20:28:43 +0100

    Day 07

---
+++ 07.py
@@ -0,0 +1,38 @@
+from typing import Callable
+
+
+with open("07-input.txt") as input_file:
+    crab_positions = [int(pos) for pos in input_file.readline().split(",")]
+
+
+print("part 1")
+
+
+def get_minimum_fuel_usage(get_fuel_usage: Callable[[int, int], int]):
+    return min(
+        sum(
+            get_fuel_usage(crab_position, to_position)
+            for crab_position in crab_positions
+        )
+        for to_position in range(min(crab_positions), max(crab_positions) + 1)
+    )
+
+
+def get_fuel_usage_one(crab_position: int, to_position: int):
+    return abs(to_position - crab_position)
+
+
+print(f"Minimum fuel usage: {get_minimum_fuel_usage(get_fuel_usage_one)}")
+
+
+print("part 2")
+
+
+def get_fuel_usage_two(crab_position: int, to_position: int):
+    distance = abs(to_position - crab_position)
+
+    # https://en.wikipedia.org/wiki/Triangular_number
+    return int((distance * (distance + 1)) / 2)
+
+
+print(f"Minimum fuel usage: {get_minimum_fuel_usage(get_fuel_usage_two)}")
